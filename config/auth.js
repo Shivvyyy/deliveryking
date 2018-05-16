c45828c45d85ba354f8cd92287bbf425175173ff
Shivvyyy <smarty.shibyan@gmail.com>
2018-05-16 19:59:43 +0530

    facebook auth

--- config/auth.js
+++ config/auth.js
@@ -6,7 +6,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'        : '195130731096693', // your App ID
         'clientSecret'    : 'd5bdbb3bf0603f54bb287e24f3ae3db9', // your App Secret
-        'callbackURL'     : 'http://localhost:5000/auth/facebook/callback',
+        'callbackURL'     : 'http://deliverykings.co.in/auth/facebook/callback',
         'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
         'profileFields'   : ['id', 'email', 'name'] // For requesting permissions from Facebook API
 
