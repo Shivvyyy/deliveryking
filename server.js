07149ce9b54eaa954d8ffa91e0627751971585ee
Shivvyyy <smarty.shibyan@gmail.com>
2018-05-17 18:15:40 +0530

    cors fix

--- server.js
+++ server.js
@@ -48,6 +48,20 @@ app.post('/ccavResponseHandler', function (request, response){
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+
 app.use(cookieParser());
 app.use(session({
   resave: true,
